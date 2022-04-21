92c34c9937de7835d356d079d29fdbc0ab4d4aa0
sivanandha <chillapothusiva411@gmail.com>
2022-04-21 10:33:43 +0530

    first commit

---
+++ Helloworld/src/main/java/com/example/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class TestController {
+	
+@RequestMapping(value="/welcome")
+	public ModelAndView first()
+	{
+		return new ModelAndView("welcome");
+		
+	}
+}
